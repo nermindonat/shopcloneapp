a4aacbb78a5cd8f7cbee38ef2383f99d2ef7911b
Nermin Donat <70170606+nermindonat@users.noreply.github.com>
2022-08-25 23:09:18 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// mobiscroll.card('.product-cards .card', {
+//     lang: 'tr',
+//     theme: 'windows',
+//     themeVariant: 'light'
+// });
+
+// mobiscroll.scrollview('.product-cards', {
+//     lang: 'tr',
+//     theme: 'windows',
+//     themeVariant: 'light',
+//     layout: 'fixed',
+//     itemWidth: 134,
+//     snap: false
+// });
